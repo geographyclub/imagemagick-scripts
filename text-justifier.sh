1251a6221c0435fb9f68126bfb4ca72704c06bb9
geographyclub <emailstevekim@gmail.com>
2021-09-15 14:30:49 -0400

    update

--- text-justifier.sh
+++ text-justifier.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-file='/home/steve/Downloads/tmp/output1.csv'
-dir='/home/steve/Downloads/tmp'
-rm -f /home/steve/Downloads/tmp/*
+file='/home/steve/data/tmp/select.csv'
+rm -f /home/steve/data/tmp/*.ppm
 
-echo "export from psql..."
-psql -d grids -t -A -F"," -c "SELECT * FROM places WHERE sov0name IN ('Italy') AND scalerank IN (0,1,2,3,4)" > ${file}
+# select data
+psql -d world -c "\COPY (SELECT a.field_2, a.field_3, a.field_4, a.field_5, round(a.field_7), round(a.field_8) FROM airports a WHERE a.scalerank IN (2)) TO STDOUT DELIMITER E'\t'" > ${file}
 
-echo "svg..."
-cat ${file} | while read line; do 
-  id=($(echo ${line} | awk -F "," '{print $1}'))
-  words=($(echo ${line} | awk -F "," '{print $7}' | tr '-' ' '))
-  lon=($(echo ${line} | awk -F "," '{print $23}'))
-  lat=($(echo ${line} | awk -F "," '{print $22}'))
-  for ((a=0; a<${#words[@]}; a=a+1)); do
-    convert -background White -fill Black -font '/home/steve/.fonts/Google Webfonts/VT323-Regular.ttf' -size 1000x -interline-spacing 0 label:${words[a]^^} -trim +repage -resize 1000x -bordercolor White -border 10 /home/steve/Downloads/tmp/word${a}.ppm
+# make captions
+cat ${file} | while IFS=$'\t' read -a array; do 
+  name=($(echo ${array[0]} | tr '-' ' '))
+  for ((a=0; a<${#name[@]}; a=a+1)); do
+    convert -background White -fill Black -font '/home/steve/.fonts/fonts-master/ofl/ibmplexmono/IBMPlexMono-SemiBold.ttf' -size 1000x -interline-spacing 0 label:${name[a]^^} -trim +repage -resize 1000x -bordercolor White -border 10 /home/steve/data/tmp/${array[3]}${a}.ppm
   done
-  convert -append $(ls -v /home/steve/Downloads/tmp/word*.ppm) /home/steve/Downloads/tmp/id_${id}.ppm
-  potrace --progress -b svg --alphamax 1.0 --color \#000000 --opttolerance 0.2 --turdsize 0 --turnpolicy min --unit 10 --output ${dir}/id_${id}.svg /home/steve/Downloads/tmp/id_${id}.ppm
+  convert -append $(ls -v /home/steve/data/tmp/${array[3]}*.ppm) /home/steve/data/tmp/${array[3]}_name.ppm
 done
 
-echo "color..."
+cat ${file} | while IFS=$'\t' read -a array; do 
+  convert -background White -fill Black -font '/home/steve/.fonts/fonts-master/ofl/ibmplexmono/IBMPlexMono-SemiBold.ttf' -size 1000x -interline-spacing 0 label:${array[3]^^} -trim +repage -resize 1000x -bordercolor White -border 10 /home/steve/data/tmp/${array[3]}_id.ppm
+  convert -append /home/steve/data/tmp/${array[3]}_id.ppm /home/steve/data/tmp/${array[3]}_name.ppm /home/steve/data/tmp/${array[3]}_final.ppm
+done
+
+# vectorize
+potrace --progress -b svg --alphamax 1.0 --color \#000000 --opttolerance 0.2 --turdsize 0 --turnpolicy min --unit 10 --output ${dir}/id_${id}.svg /home/steve/data/tmp/id_${id}.ppm
+
+# color
 rm -f ${dir}/*b.svg
 ls ${dir}/*.svg | while read file; do 
   cat ${file} | sed 's/fill=\"#000000\"/fill=\"#ffffff\"/g' | sed 's/stroke=\"none\"/stroke=\"#000000\" stroke-width=\"10\" vector-effect=\"non-scaling-stroke\"/g' > ${file%.svg}b.svg
